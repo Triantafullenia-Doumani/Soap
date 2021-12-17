fcd39cae137f852c28e55c44ba735420d4a1b66c
Triantafyllenia Doumani <Triantafulleniad@gmail.com>
2021-12-17 13:30:54 +0100

    Update server.php

--- server.php
+++ server.php
@@ -4,8 +4,8 @@ ini_set("soap.wsdl_cache_enabled","0");
 // model, which uses in web service functions as parameter
 class Book
 {
-	public $name;
-	public $year;
+        public $name;
+        public $year;
 }
 
 /**
@@ -15,25 +15,25 @@ class Book
  */
 function bookYear($book)
 {
-	// list of the books
-	$_books=[
-		['name'=>'test 1','year'=>2011],
-		['name'=>'test 2','year'=>2012],
-		['name'=>'test 3','year'=>2013],
-	];
-	// search book by name
-	foreach($_books as $bk)
-		if($bk['name']==$book->name)
-			return $bk['year']; // book found
+        // list of the books
+        $_books=[
+                ['name'=>'test 1','year'=>2011],
+                ['name'=>'test 2','year'=>2012],
+                ['name'=>'test 3','year'=>2013],
+        ];
+        // search book by name
+        foreach($_books as $bk)
+                if($bk['name']==$book->name)
+                        return $bk['year']; // book found
 
-	return 0; // book not found
+        return 0; // book not found
 }
 
 // initialize SOAP Server
 $server=new SoapServer("test.wsdl",[
-	'classmap'=>[
-		'book'=>'Book', // 'book' complex type in WSDL file mapped to the Book PHP class
-	]
+        'classmap'=>[
+                'book'=>'Book', // 'book' complex type in WSDL file mapped to the Book PHP class
+        ]
 ]);
 
 // register available functions
@@ -41,6 +41,5 @@ $server->addFunction('bookYear');
 
 while(1){
 // start handling requests
-	$server->handle();
+        $server->handle();
 };
-
